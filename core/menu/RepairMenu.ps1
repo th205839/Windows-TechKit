2bba6f8bb1d480de330d0bd3a0f6ed3e2477fe49
th205839 <thiago.correadealmeida76@gmail.com>
2026-08-04 10:18:25 -0300

    feat(repair): add repair menu integration

---
+++ core/menu/RepairMenu.ps1
@@ -0,0 +1,8 @@
+function Start-RepairMenu {
+    Write-Host ""
+    Write-Host "Windows Repair"
+    Write-Host "1 - Full Repair"
+    Write-Host "2 - SFC"
+    Write-Host "3 - DISM"
+    Write-Host "4 - Check Disk"
+}
