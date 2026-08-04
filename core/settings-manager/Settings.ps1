146f6eca4c24be4b9bc5ab5ab877af98b566c155
th205839 <thiago.correadealmeida76@gmail.com>
2026-08-04 13:51:57 -0300

    feat(core): improve settings manager foundation

--- core/settings-manager/Settings.ps1
+++ core/settings-manager/Settings.ps1
@@ -2,6 +2,11 @@
 
 $script:TechKitSettings = @{}
 
+function Get-TechKitSetting {
+    param($Name)
+    return $script:TechKitSettings[$Name]
+}
+
 function Set-TechKitSetting {
     param($Name,$Value)
     $script:TechKitSettings[$Name] = $Value
